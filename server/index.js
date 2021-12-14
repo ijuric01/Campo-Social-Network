ce507c20283d8b623074767a29141e691d81f5fe
Iva Jurić <48961933+ijuric01@users.noreply.github.com>
2021-12-14 09:49:42 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("app is running");
 });
 const CONNECTION_URL =
-  "mongodb+srv://ijuric01:Io71860F@cluster0.kne4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://ijuric01:xxxxxxx@cluster0.kne4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
